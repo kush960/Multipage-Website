6a92426b2608a2d925e20cb1b9156e57bf960c62
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-07-20 19:23:17 +0530

    hero-section update

--- react-website/src/components/HeroSection.jsx
+++ react-website/src/components/HeroSection.jsx
@@ -19,12 +19,62 @@ import { Button } from "../styles/button";
         {/* Below div is for image */}
         <div className="section-hero-image">
             <picture>
-                <img src="./" alt="" ></img>
+                <img src="./images/hero.jpg" alt="" srcSet="" ></img>
             </picture>
         </div>
     </div>
    </Wrapper>)
  };
  
- const Wrapper = styled.section``;
+ const Wrapper = styled.section`
+  padding: 9rem 0;
+
+  .section-hero-data {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .btn {
+    max-width: 16rem;
+  }
+
+  .hero-top-data {
+    text-transform: uppercase;
+    font-weight: 500;
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme.colors.helper};
+  }
+
+  .hero-heading {
+    text-transform: uppercase;
+    font-size: 6.4rem;
+  }
+
+  .hero-para {
+    margin-top: 1.5rem;
+    margin-bottom: 3.4rem;
+    max-width: 60rem;
+  }
+
+  .section-hero-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  picture {
+    text-align: center;
+  }
+
+  .hero-img {
+    max-width: 80%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid {
+      gap: 7.2rem;
+    }
+  }
+ `;
  export default HeroSection;
