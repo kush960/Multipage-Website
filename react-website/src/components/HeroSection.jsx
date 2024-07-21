dc999bcc92886e23fe0b18a3ee2a282656aa932a
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-07-21 12:24:44 +0530

    globalstyle-css update

--- react-website/src/components/HeroSection.jsx
+++ react-website/src/components/HeroSection.jsx
@@ -2,17 +2,18 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../styles/button"; 
  const HeroSection = () => {
-   return  (<Wrapper>
-    <div className="container grid grid-two-colimn">
+   return  (
+    <Wrapper>
+        <div className="container grid grid-two-column">
          {/* Below div is for content */}
-        <div className="section-hero-data">
-            <p className="hero-top-data">THIS IS ME</p>
-            <h1 className="hero-heading"> KUSHAGRA ANKIT </h1>
-            <p className="hero-para"> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Nostrum numquam blanditiis sit a, doloremque ipsum velit quo consequatur fugit vero in error. 
-                Labore earum, id atque ipsa ducimus cum distinctio.
-            </p>
-        </div>
+            <div className="section-hero-data">
+                <p className="hero-top-data">THIS IS ME</p>
+                <h1 className="hero-heading"> KUSHAGRA ANKIT </h1>
+                <p className="hero-para"> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                   Nostrum numquam blanditiis sit a, doloremque ipsum velit quo consequatur fugit vero in error. 
+                   Labore earum, id atque ipsa ducimus cum distinctio.
+                </p>
+            </div>
         <Button className="btn hireme-btn">
             <NavLink to="/contact"> GO </NavLink>
         </Button>
