4a5526cc858320888ef5065c812898b1248a9871
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-07-31 23:22:06 +0530

    heading and api buildup in services

--- react-website/src/Services.jsx
+++ react-website/src/Services.jsx
@@ -5,8 +5,10 @@ const Services = () => {
   const { services } = useGlobalContext();
   console.log(services);
   return  <Wrapepr classname="section">
-    <h2> Our Services</h2>
-  </Wrapepr>
-}
+    <h2 className="common-heading"> Our Services</h2>
+  </Wrapepr>;
+};
+
+const Wrapepr = styled.section``;
 
 export default Services;
