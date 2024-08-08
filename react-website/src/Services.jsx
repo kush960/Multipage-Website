ef3b8cbdf7cb84b5b4446666853b9fa540b89f5c
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-08-08 23:20:30 +0530

    service-update

--- react-website/src/Services.jsx
+++ react-website/src/Services.jsx
@@ -40,35 +40,17 @@ background-color: ${({ theme }) => theme.colors.bg};
 .container{
 max-width: 120 rem;
 }
-
  .card {
     border: 0.1rem solid rgb(170 170 170 / 40%);
-    .card-data {
-      padding: 0 2rem;
+    .card-data{
+    padding: 0 2rem;
     }
-
-    h3 {
-      margin: 2rem 0;
-      font-weight: 300;
-      font-size: 2.4rem;
+    h3{
+    margin: 2rem 0;
+    font-weight: 300;
+    font-size: 2.4 rem
     }
-    .btn {
-      margin: 2rem auto;
-      background-color: rgb(0 0 0 / 0%);
-      border: 0.1rem solid rgb(98 84 243);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: rgb(98 84 243);
-      font-size: 1.4rem;
-
-      &:hover {
-        background-color: rgb(98 84 243);
-        color: #fff;
-      }
-    }
-  }
-
+}
   figure {
     width: auto;
     display: flex;
@@ -101,19 +83,6 @@ max-width: 120 rem;
       transition: all 0.2s linear;
     }
   }
-     @media (max-width: ${({ theme }) => theme.media.tab}) {
-    .grid-three-column {
-      grid-template-columns: 1fr 1fr;
-    }
-  }
-
-  @media (max-width: ${({ theme }) => theme.media.mobile}) {
-    .grid-two-column,
-    .grid-three-column {
-      grid-template-columns: 1fr;
-    }
-  }
-
 `;
 
 export default Services;
